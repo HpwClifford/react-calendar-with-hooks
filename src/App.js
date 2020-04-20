06ef54582a374707d83c19b7000347175b0b4077
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-04-19 22:26:48 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // import Week from './Week.js';
 
 var url = 'http://slack-server.elasticbeanstalk.com/calendar/NY/18'
 
-export default class App extends Component {
-  contstructor() {
-    this.state = 1;
-  }
-
-  render() {
-    // const arr = Object.keys(this.state).map(elem =>  <Week data={elem}/>);
-    console.log(this.state, '<- state');
-    if (this.state) console.log(this.state.data);
-
-    return (
-      <div>
-       {}
-      </div>
-    )
-  }
-
-  componentDidMount() {
-    fetch(url)
-    .then(data => data.json())
-    .then(data => {
-      this.setState({
-        data: data,
-      });
-    });
-  }
+function App() {
+  const [todos, setTodos]
 }
+
+export default App;
+
