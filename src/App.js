34d21b35ae7c522513e1417ed9614342dd220315
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-04-19 21:19:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import Week from './Week.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var url = 'http://slack-server.elasticbeanstalk.com/calendar/NY/18'
+
+export default class App extends Component {
+  contstructor() {
+    this.state = 1;
+  }
+
+  render() {
+    // const arr = Object.keys(this.state).map(elem =>  <Week data={elem}/>);
+    console.log(this.state, '<- state');
+    if (this.state) console.log(this.state.data);
+
+    return (
+      <div>
+       {}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(data => data.json())
+    .then(data => {
+      this.setState({
+        data: data,
+      });
+    });
+  }
 }
-
-export default App;
